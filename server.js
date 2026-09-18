7e78ad3e3b308c23d8fa37118d9e84fc1a56cab9
yytabares-glitch <yytabares@gmail.com>
2026-09-18 17:00:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const GEODIS_LOGIN = '2235572$';
+const GEODIS_LOGIN = 'IZACLOGNC';
 const GEODIS_KEY   = '35a654a41c7045d8adbea5170210cdf8';
 const LANG         = 'fr';
 
